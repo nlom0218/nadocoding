c31eddc83b43f6c3c7ecae8704ba58eaae30781b
nlom0218 <nlom0218@naver.com>
2022-03-06 10:23:26 +0900

    실수형 변수 float, double

--- printfScanf.c
+++ printfScanf.c
@@ -2,9 +2,16 @@
 
 int main(void)
 {
-  int age = 12;
-  printf("%d\n", age);
-  age = 13;
-  printf("%d\n", age);
+  // 정수형
+  // int age = 12;
+  // printf("%d\n", age);
+  // age = 13;
+  // printf("%d\n", age);
+
+  // 실수형
+  float f = 46.589f;
+  printf("%.2f\n", f);
+  double d = 4.428;
+  printf("%.2lf\n", d);
   return 0;
 }
