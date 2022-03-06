48e6d0bac08349a67d6eccb90bcaa3e3c8149c03
nlom0218 <nlom0218@naver.com>
2022-03-06 10:48:03 +0900

    상수 const, printf

--- printfScanf.c
+++ printfScanf.c
@@ -9,9 +9,19 @@ int main(void)
   // printf("%d\n", age);
 
   // 실수형
-  float f = 46.589f;
-  printf("%.2f\n", f);
-  double d = 4.428;
-  printf("%.2lf\n", d);
+  // float f = 46.589f;
+  // printf("%.2f\n", f);
+  // double d = 4.428;
+  // printf("%.2lf\n", d);
+
+  // 상수
+  // const int YEAR = 2000;
+  // printf("태어난 년도: %D\n", YEAR);
+
+  // printf
+  int add = 3 + 7; // 10
+  printf("3 + 7 = %d\n", add);
+  printf("%d * %d = %d\n", 30, 79, 30 * 79);
+
   return 0;
 }
