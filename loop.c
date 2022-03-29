9c057f1f8fd1cd74e24a11db2ab335cebf4cad37
nlom0218 <nlom0218@naver.com>
2022-03-29 22:53:51 +0900

    loop for

---
+++ loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+  // printf("Hello World\n");
+  // int a = 10;
+  // printf("a 는 %d\n", a++);
+  // printf("a 는 %d\n", ++a);
+  // printf("a 는 %d\n", a);
+
+  int i = 1;
+  for (int i = 0; i < 10; i++)
+  {
+    printf("Hello Word\n");
+  }
+
+  return 0;
+}
