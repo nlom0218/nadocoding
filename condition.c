5d0c4eda64ec27e061b4969bd13f2d6b4d25fb03
nlom0218 <nlom0218@naver.com>
2022-04-01 17:34:56 +0900

    Break Continue

--- condition.c
+++ condition.c
@@ -13,24 +13,47 @@ int main(void)
   // }
 
   // 초(8~13) 중(14~16) 고(17~19)
-  int age;
-  printf("몇살이나요?");
-  scanf("%d", &age);
-  if (age >= 8 && age <= 13)
+  // int age;
+  // printf("몇살이나요?");
+  // scanf("%d", &age);
+  // if (age >= 8 && age <= 13)
+  // {
+  //   printf("초등학생입니다.\n");
+  // }
+  // else if (age >= 14 && age <= 16)
+  // {
+  //   printf("중학생입니다.\n");
+  // }
+  // else if (age >= 17 && age <= 19)
+  // {
+  //   printf("고등학생입니다.\n");
+  // }
+  // else
+  // {
+  //   printf("학생이 아닌가봐요.\n");
+  // }
+
+  // for (int i = 0; i < 30; i++)
+  // {
+  //   if (i > 5)
+  //   {
+  //     printf("나머지 학생은 집에 가세요\n");
+  //     break;
+  //   }
+  //   printf("%d번 학생은 조별 발표 준비를 하세요\n", i + 1);
+  // }
+
+  for (int i = 0; i <= 30; i++)
   {
-    printf("초등학생입니다.\n");
-  }
-  else if (age >= 14 && age <= 16)
-  {
-    printf("중학생입니다.\n");
-  }
-  else if (age >= 17 && age <= 19)
-  {
-    printf("고등학생입니다.\n");
-  }
-  else
-  {
-    printf("학생이 아닌가봐요.\n");
+    if (i >= 6 && i <= 10)
+    {
+      if (i == 7)
+      {
+        printf("%d번 학생은 결석입니다.\n", i);
+        continue;
+      }
+      printf("%d번 학생은 조별 발표를 준비를 하세요\n", i);
+    }
   }
 
   return 0;
